67b90cdbfbccc79e576173b2d2aaf8778591ad64
elsuizo <mnoblia@disroot.org>
2020-08-18 22:05:46 -0300

    simplify the usage

--- src/main.rs
+++ src/main.rs
@@ -48,18 +48,8 @@ fn write_file(out: &mut dyn Write, content: &str) -> std::io::Result<()> {
 fn main() -> Result<(), Box<dyn Error>> {
     // TODO(elsuizo:2020-08-16): maybe the filename could read from command line
 
-    // read the command line input we spect the catkin workspace path
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 2 {
-        panic!("error you should provide a catkin worspace path");
-    }
-    // cd to the current workspace
-    let catkin_workspace_path = Path::new(&args[1]);
-    env::set_current_dir(&catkin_workspace_path)?;
-    // read the config values
-    let path_config = Path::new("config.yaml");
-    let string_config = read_to_string(path_config)?;
+    let current_dir = env::current_dir()?;
+    let string_config = read_to_string("config.yaml")?;
     let user_config: Config = serde_yaml::from_str(&string_config)?;
 
     // create the workspace Cargo.toml file filling the template
@@ -67,10 +57,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut workspace_file = File::create("Cargo.toml")?;
     write_file(&mut workspace_file, ws.render()?.as_str())?;
 
+    // create the .config dir and the config file
+    create_dir_all(current_dir.join(".cargo"))?;
+    let mut config_cargo = File::create(".cargo/config")?;
+    write_file(&mut config_cargo, CONFIG_STRING)?;
+
     // create all the nodes
     for node in &user_config.nodes {
         let cmake_template = CMakeTemplate {node: node.to_string()};
-        env::set_current_dir(catkin_workspace_path.join("src"))?;
+        env::set_current_dir(current_dir.join("src"))?;
 
         Command::new("cargo")
             .args(&["new", node.as_str()])
@@ -81,18 +76,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         let mut cmake_file = File::create(node_path.join("CMakeLists.txt"))?;
         write_file(&mut cmake_file, cmake_template.render()?.as_str())?;
 
-        // create the package.xml file in the node
+        // // create the package.xml file in the node
         let package_template = PackageTemplate {node: node.to_string(), version: user_config.version.clone()};
         let mut package_file = File::create(node_path.join("package.xml"))?;
         write_file(&mut package_file, package_template.render()?.as_str())?;
 
     }
-
-    // create the .config dir and the config file
-    env::set_current_dir(catkin_workspace_path)?;
-    create_dir_all(".cargo")?;
-    let mut config_cargo = File::create(".cargo/config")?;
-    write_file(&mut config_cargo, CONFIG_STRING)?;
 
     println!("{:} project generated!!!", user_config.project_name);
 
